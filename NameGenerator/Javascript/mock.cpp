f257ecb96b559af162007ed35d4b04c7640a34d4
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2019-02-01 15:07:30 -0800

    Added name generator

---
+++ NameGenerator/Javascript/mock.cpp
@@ -0,0 +1,283 @@
+
+			 
+
+
+			public	 static string YoutubeLink()
+			string q
+				"a",
+				"b",
+				"c",
+				"d",
+				"e",
+				"f",
+				"g",
+				"h",
+				"i",
+				"j",
+				"k",
+				"l",
+				"m",
+				"n",
+				"o",
+				"p",
+				"q",
+				"r",
+				"s",
+				"t",
+				"u",
+				"v",
+				"w",
+				"x",
+				"y",
+				"z",
+				"Q",
+				"W",
+				"E",
+				"R",
+				"T",
+				"Y",
+				"U",
+				"I",
+				"O",
+				"P",
+				"A",
+				"S",
+				"D",
+				"F",
+				"G",
+				"H",
+				"J",
+				"K",
+				"L",
+				"Z",
+				"X",
+				"C",
+				"V",
+				"B",
+				"N",
+				"M",
+				"1",
+				"2",
+				"3",
+				"4",
+				"5",
+				"6",
+				"7",
+				"8",
+				"9",
+			 62	"9",
+
+
+			return q
+			
+			
+
+
+			public	 static string African(string nam"text")
+			string q,w,e
+			//Sauhmedanu
+			/*
+			Sauh
+			Saji
+			Kawah
+			Kemh
+			Suh
+			Aleh
+			Wana
+			Waka
+			Awah
+			Umwa
+			Kata
+			Umuh
+
+			Meda
+			unu
+			wa
+			ahun
+			iwi
+			hali
+			lo
+			ulu
+
+
+
+			nu
+			na
+			la
+			lu
+			le
+			awa
+			uwa
+			ola
+			ole
+			an
+			upa
+			un
+
+
+
+			*/
+
+			 1	"sauh",
+			 2	"saji",
+			 3	"kawah",
+			 4	"kemh",
+			 5	"la",
+			 6	"aleh",
+			 7	"wana",
+			 8	"wala",
+			 9	"waka",
+			 10	"gawa",
+			 11	"awah",
+			 12	"umwa",
+			 13	"umha",
+			 14	"umka",
+			 15	"kata",
+			 16	"aku",
+			 17	"meka",
+			 18	"sali",
+			 19	"sala",
+			 20	"uku",
+			 21	"kali",
+			 22	,
+			 23	,
+			 24	,
+			 25	,
+			 26	,
+			 27	,
+			 28	,
+			 29	,
+			 30	,
+			 31	,
+			 32	,
+			 33	,
+			 34	,
+			 35	,
+			 36	,
+			 37	,
+			 38	,
+			 39	,
+			 40	,
+			 41	,
+			 42	,
+			 43	,
+			 44	,
+			 45	,
+			 46	,
+			 47	,
+			 48	,
+			 49	,
+			 50	,
+
+
+			 1	"meda",
+			 2	"unu",
+			 3	"ahun",
+			 4	"iwi",
+			 5	"hali",
+			 6	"lo",
+			 7	"ulu",
+			 8	"wa",
+			 9	"wam",
+			 10	"nag",
+			 11	"boma",
+			 12	"imbaba",
+			 13	"dau",
+			 14	"wakuna",
+			 15	"sau",
+			 16	,
+			 17	,
+			 18	,
+			 19	,
+			 20	,
+			 21	,
+			 22	,
+			 23	,
+			 24	,
+			 25	,
+			 26	,
+			 27	,
+			 28	,
+			 29	,
+			 30	,
+			 31	,
+			 32	,
+			 33	,
+			 34	,
+			 35	,
+			 36	,
+			 37	,
+			 38	,
+			 39	,
+			 40	,
+			 41	,
+			 42	,
+			 43	,
+			 44	,
+			 45	,
+			 46	,
+			 47	,
+			 48	,
+			 49	,
+			 50	,
+
+
+			 1	"nu",
+			 2	"na",
+			 3	"la",
+			 4	"lu",
+			 5	"le",
+			 6	"li",
+			 7	"awa",
+			 8	"uwa",
+			 9	"anwa",
+			 10	"anu",
+			 11	"mali",
+			 12	"ole",
+			 13	"ola",
+			 14	"an",
+			 15	"upa",
+			 16	"una",
+			 17	"auna",
+			 18	"dan",
+			 19	"kana",
+			 20	,
+			 21	,
+			 22	,
+			 23	,
+			 24	,
+			 25	,
+			 26	,
+			 27	,
+			 28	,
+			 29	,
+			 30	,
+			 31	,
+			 32	,
+			 33	,
+			 34	,
+			 35	,
+			 36	,
+			 37	,
+			 38	,
+			 39	,
+			 40	,
+			 41	,
+			 42	,
+			 43	,
+			 44	,
+			 45	,
+			 46	,
+			 47	,
+			 48	,
+			 49	,
+			 50	,
+
+			 1	 return q+w+e
+			 2	 return q+w
+			 3	 return q+e
+			 4	 return e
+			 5	 return q
+			 8	 return q
+			 6	 return q+w
+			 7	 return q+e
